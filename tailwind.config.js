d5479b4a5a44f46aadacd1c90a1fb29eb462559d
Shimin Zhang <john.shimin.zhang@gmail.com>
2021-04-27 11:17:32 -0600

    add safelist for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist: ['text-red-500', 'text-green-500', 'text-blue-500', 'text-yellow-500', 'text-indigo-500']
+  } ,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
