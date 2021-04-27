2536a508fde521fe88c20d28d9aa046545206220
Shimin Zhang <john.shimin.zhang@gmail.com>
2021-04-27 11:08:54 -0600

    remove custom color, use default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      blue: colors.blue,
-      green: colors.green,
-      yellow: colors.amber
-    },
     extend: {
     },
   },
