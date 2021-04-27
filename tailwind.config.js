80ed0f149597653aef22479b4f207b5f3a357272
Shimin Zhang <john.shimin.zhang@gmail.com>
2021-04-27 11:22:43 -0600

    add options config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   purge: {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    safelist: ['text-red-500', 'text-green-500', 'text-blue-500', 'text-yellow-500', 'text-indigo-500']
-  } ,
+    options: {
+      safelist: ['text-red-500', 'text-green-500', 'text-blue-500', 'text-yellow-500', 'text-indigo-500']
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
